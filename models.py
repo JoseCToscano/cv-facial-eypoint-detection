c54abc156e9db434be5f903cccb1f05c7e61074f
Jose Toscano <josetoscano@atratopago.com>
2022-12-15 12:25:59 -0600

    Adding first attempt to build CNN to find face keypoints. Built and based architecture using AlexNet's architecture as reference in a much simpler version, did not add LocalReponseNormalization as tge original architecture. First attempt was to try the basic architecture and see results. Results: - Implemented training with MSE and MAE as Loss funcitons and tryed using Adam and SGD as the optimizers - Did not notice any signidicant change when alternating the loss functions and optimizers - Results indicate a clear overfitting of the data after just a few batch forward passes within the first epoch - Tried with 10-20-50 for batch sizes, with similar results Conclusion: - The AlexNet architecture may not be the best choice for face keypoint detection. - Will make additional attempts with diferent and more modern architectures like VGG or ResNe before attempting dataaugmentation strategies or more intense optimizer and param optimization

--- models.py
+++ models.py
@@ -3,6 +3,7 @@
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
+import torch.nn.init as init
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
@@ -13,25 +14,71 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## Definition all the layers of this CNN:
+        
         ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11, stride=4),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(96, 256, kernel_size=5, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
         
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(256, 384, kernel_size=3, stride=1),
+            nn.ReLU())
+        
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(384, 384, kernel_size=3, stride=1),
+            nn.ReLU())
+        
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
+        
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(256, 4096),
+            nn.ReLU())
+        
+        self.fc2 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        # finally, create 136 output channels, 2 for each of the 68 keypoint (x, y) pairs        
+        self.fc3= nn.Sequential(
+            nn.Linear(4096, 136))
+
+        # Weight initializarion using Xavier's method
+        self.initialize_weights()
 
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+
+        # prep for linear layer
+        # this line of code is the equivalent of Flatten in Keras
+        x = x.view(x.size(0), -1)
         
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+    
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                print(m)
+                init.xavier_uniform_(m.weight)
